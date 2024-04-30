bb59d5bdd30e4edb9ba737c6d931ae0d393f8c6a
Ankit Kumar <86267078+iamankit080@users.noreply.github.com>
2024-04-30 13:37:16 +0530

    Add files via upload

---
+++ NFTForge Code/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <div id="footer">
+      <footer>
+        <Container fluid="md">
+          <Row>
+            <Col>
+              <p>
+                Project for Chandigarh University Semester-8 by:
+                Ankit Kumar, Gurpreet, Tijil, Yash Kumar
+              </p>
+            </Col>
+            <Col>
+              <p>Copyright ⓒ {year}</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
